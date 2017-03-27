e636d026b674c1c67813c858b5affb4c0160c642
oshanker <oshanker@hotmail.com>
2017-03-27 14:52:02 -0700

    Add test for zeta eval

---
+++ oldriemann/src/test/riemann/RiemannTest.java
@@ -0,0 +1,46 @@
+package riemann;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RiemannTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRiemannDoubleLong() {
+		double[] args = {1.87383225, 2.13054990,        
+		        2.38726754,   2.64398519        };
+		double[] vals = {4.023671,42.85232072, -4.52434707, 
+				-2.84388877};
+		long init = 0;
+		for (int i = 0; i < args.length; i++) {
+			double zeta = Riemann.riemann(args[i], 267653395647L);
+			System.out.println(args[i] + ":" +zeta + ":" + Math.abs(zeta-vals[i]));
+			assertTrue(i + ":" + Math.abs(zeta-vals[i]), Math.abs(zeta-vals[i])<0.0000005);
+			if(i==0){init = System.currentTimeMillis();}
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("calc for " + (args.length-1) + " " + (end - init) + "ms");
+	}
+
+}
