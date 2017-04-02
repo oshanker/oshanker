a9aa0aea2fa80001a827553990b5d964ae6c4b70
oshanker <oshanker@hotmail.com>
2017-04-01 19:16:53 -0700

    configurable length for correlation samples

--- oldriemann/src/main/riemann/Conjectures.java
+++ oldriemann/src/main/riemann/Conjectures.java
@@ -7,15 +7,15 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class Conjectures {
-	public static int[] readItems(String filename, int N)
+	public static int[] readItems(String filename, int sampleLength, int N)
 			throws FileNotFoundException, IOException {
 		/**
 		 * ranked list of colos for overall cluster.
 		 */
 		BufferedReader zeroIn = new BufferedReader(new FileReader(filename));
 		int count = 0;
-		int[] signumSet = new int[8];
-		int[] signum = new int[3];
+		int[] signumSet = new int[1<<sampleLength];
+		int[] signum = new int[sampleLength];
 		while (count < N) {
 			String input = null;;
 			while ((input = zeroIn.readLine()) != null) {
@@ -27,11 +27,15 @@ public class Conjectures {
 				}
 				String[] parsed = input.trim().split(",");
 				double zeta = Double.parseDouble(parsed[1].trim());
-				signum[0] = signum[1];
-				signum[1] = signum[2];
-				signum[2] = (int) ((1+Math.signum(zeta))/2);
-				if(count>=3){
-					int idx = 4*signum[0] + 2*signum[1] + signum[2];
+				for (int i = 0; i < signum.length-1; i++) {
+					signum[i] = signum[i+1];
+				}
+				signum[signum.length-1] = (int) ((1+Math.signum(zeta))/2);
+				if(count>=sampleLength){
+					int idx = 0;
+					for (int i = 0; i < signum.length; i++) {
+						idx += signum[i]*(1<<(signum.length-1-i));
+					}
 					signumSet[idx]++;
 				}
 				count++;
@@ -42,9 +46,10 @@ public class Conjectures {
 	}
 
 	public static void main(String[] args) throws Exception {
-		int[] signumSet = readItems("data/zeta12.csv", 100000);
+		int[] signumSet = readItems("data/zeta12.csv", 3, 100000);
 		System.out.println(Arrays.toString(signumSet));
 		//[6701, 7926, 27342, 8001, 7926, 27417, 8000, 6684]
+		//[14627, 35343, 35343, 14685]
 	}
 
 }
