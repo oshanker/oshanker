66e589b9177576bae86a2a643ee1590cbe2d1c1e
oshanker <oshanker@hotmail.com>
2017-04-03 11:14:26 -0700

    calculate various versions of correlation samples

--- oldriemann/src/main/riemann/Conjectures.java
+++ oldriemann/src/main/riemann/Conjectures.java
@@ -7,18 +7,20 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class Conjectures {
-	public static int[] readItems(String filename, int sampleLength, int N)
+	int[] signumSet;
+	public int[] readItems(String filename, int sampleLength, 
+			int sampleOffset, int sampleIncrement, int N)
 			throws FileNotFoundException, IOException {
 		/**
 		 * ranked list of colos for overall cluster.
 		 */
 		BufferedReader zeroIn = new BufferedReader(new FileReader(filename));
 		int count = 0;
-		int[] signumSet = new int[1<<sampleLength];
-		boolean[] printed = new boolean[1<<sampleLength];
+		signumSet = new int[1<<sampleLength];
+//		boolean[] printed = new boolean[1<<sampleLength];
 		int[] signum = new int[sampleLength];
 		while (count < N) {
-			String input = null;;
+			String input = null;
 			while ((input = zeroIn.readLine()) != null) {
 				if(input == null || input.trim().length()==0){
 					break;
@@ -32,16 +34,17 @@ public class Conjectures {
 					signum[i] = signum[i+1];
 				}
 				signum[signum.length-1] = (int) ((1+Math.signum(zeta))/2);
-				if(count>=sampleLength){
+				int checkpointForStoring = count+1-sampleLength-sampleOffset;
+				if(checkpointForStoring>=0 && checkpointForStoring%sampleIncrement == 0){
 					int idx = 0;
 					for (int i = 0; i < signum.length; i++) {
 						idx += signum[i]<<(signum.length-1-i);
 					}
 					signumSet[idx]++;
-					if(!printed[idx]){
-						System.out.println(idx + ":" + Arrays.toString(signum));
-						printed[idx] = true;
-					}
+//					if(!printed[idx]){
+//						System.out.println(idx + ":" + Arrays.toString(signum));
+//						printed[idx] = true;
+//					}
 				}
 				count++;
 			}
@@ -52,10 +55,22 @@ public class Conjectures {
 
 	public static void main(String[] args) throws Exception {
 		int sampleLength = 3;
-		int[] signumSet = readItems("data/zeta12.csv", sampleLength, 100000);
-		System.out.println(Arrays.toString(signumSet));
-		//[6701, 7926, 27342, 8001, 7926, 27417, 8000, 6684]
+		int sampleOffset = 0; 
+		int sampleIncrement = 1;
+		Conjectures instance = new Conjectures();
+		instance.readItems("data/zeta12.csv", sampleLength, sampleOffset, sampleIncrement,
+				100000);
+		System.out.println(Arrays.toString(instance.signumSet));
+		//[49970, 50030]
+		//[39945, 10055]
+		//[10025, 39975]
 		//[14627, 35343, 35343, 14685]
+		//[6701, 7926, 27342, 8001,  7926, 27417, 8001, 6684]
+		//[1374, 5970,   730, 1951,  5909, 26692, 2012, 5361] sampleOffset = 1
+		//[---,   --+,  -+-,  -++,   +--,   +-+,  ++-,   +++] 
+		//[5327, 1956, 26612, 6050,  2017,   725, 5989, 1323] sampleOffset = 0
+
+
 	}
 
 }
