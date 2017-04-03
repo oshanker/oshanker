6ef2c20ed8a0037d4b6c184d4aa51516669113b8
oshanker <oshanker@hotmail.com>
2017-04-03 14:30:54 -0700

    reversed does not give new information, just rearranges forward

--- oldriemann/src/main/riemann/Conjectures.java
+++ oldriemann/src/main/riemann/Conjectures.java
@@ -7,18 +7,21 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class Conjectures {
-	int[] signumSet;
-	public int[] readItems(String filename, int sampleLength, 
-			int sampleOffset, int sampleIncrement, int N)
+	int[] signumCounts;
+	
+	public static int[] reverse(int[] data) {
+		int[] reversed = new int[data.length];
+	    for (int i = 0; i < data.length; i++) {
+			reversed[i] = data[data.length-1-i];
+		}
+		return reversed;
+	}
+	
+	public static int[] readItems(String filename,  int N)
 			throws FileNotFoundException, IOException {
-		/**
-		 * ranked list of colos for overall cluster.
-		 */
+		int[] signumPoints  = new int[N];
 		BufferedReader zeroIn = new BufferedReader(new FileReader(filename));
 		int count = 0;
-		signumSet = new int[1<<sampleLength];
-//		boolean[] printed = new boolean[1<<sampleLength];
-		int[] signum = new int[sampleLength];
 		while (count < N) {
 			String input = null;
 			while ((input = zeroIn.readLine()) != null) {
@@ -30,47 +33,70 @@ public class Conjectures {
 				}
 				String[] parsed = input.trim().split(",");
 				double zeta = Double.parseDouble(parsed[1].trim());
-				for (int i = 0; i < signum.length-1; i++) {
-					signum[i] = signum[i+1];
-				}
-				signum[signum.length-1] = (int) ((1+Math.signum(zeta))/2);
+				signumPoints[count] = (int) ((1+Math.signum(zeta))/2);
+				count++;
+			}
+		}
+		zeroIn.close();
+		if(count != N){
+			throw new IllegalStateException("count " + count + ", N " + N);
+		}
+		return signumPoints;
+	}
+
+	public int[] calculateDistribution(int[] signumPoints, int sampleLength, 
+			int sampleOffset, int sampleIncrement, int N)
+			throws FileNotFoundException, IOException {
+		int count = 0;
+		signumCounts = new int[1<<sampleLength];
+//		boolean[] printed = new boolean[1<<sampleLength];
+		while (count < N) {
 				int checkpointForStoring = count+1-sampleLength-sampleOffset;
 				if(checkpointForStoring>=0 && checkpointForStoring%sampleIncrement == 0){
 					int idx = 0;
-					for (int i = 0; i < signum.length; i++) {
-						idx += signum[i]<<(signum.length-1-i);
+					for (int i = 0; i < sampleLength; i++) {
+						idx += signumPoints[count+1-sampleLength+i]<<(sampleLength-1-i);
 					}
-					signumSet[idx]++;
+					signumCounts[idx]++;
 //					if(!printed[idx]){
 //						System.out.println(idx + ":" + Arrays.toString(signum));
 //						printed[idx] = true;
 //					}
 				}
 				count++;
-			}
 		}
-		zeroIn.close();
-		return signumSet;
+		return signumCounts;
 	}
 
 	public static void main(String[] args) throws Exception {
 		int sampleLength = 3;
-		int sampleOffset = 0; 
-		int sampleIncrement = 1;
+		int sampleOffset = 1; 
+		int sampleIncrement = 2;
+		int N = 100000;
+		int[] signumPoints = readItems("data/zeta12.csv", N);
+		int[] reversedPoints = reverse(signumPoints);
 		Conjectures instance = new Conjectures();
-		instance.readItems("data/zeta12.csv", sampleLength, sampleOffset, sampleIncrement,
-				100000);
-		System.out.println(Arrays.toString(instance.signumSet));
+		instance.calculateDistribution(signumPoints, sampleLength, sampleOffset, sampleIncrement,
+				N);
+		System.out.println(Arrays.toString(instance.signumCounts));
+		Conjectures reversed = new Conjectures();
+		reversed.calculateDistribution(reversedPoints, sampleLength, sampleOffset+1, sampleIncrement,
+				N);
+		System.out.println(Arrays.toString(reversed.signumCounts));
+		//[ -,     +   ] 
 		//[49970, 50030]
 		//[39945, 10055]
 		//[10025, 39975]
-		//[14627, 35343, 35343, 14685]
-		//[6701, 7926, 27342, 8001,  7926, 27417, 8001, 6684]
-		//[1374, 5970,   730, 1951,  5909, 26692, 2012, 5361] sampleOffset = 1
+		//[ --,     -+,    +-,    ++ ] 
+		//[14627, 35343, 35343, 14686]
+		//[ 7344,  2681, 32601,  7373]
+		//[ 7283, 32662,  2742,  7313]
 		//[---,   --+,  -+-,  -++,   +--,   +-+,  ++-,   +++] 
+		//[6701, 7926, 27342, 8001,  7926, 27417, 8001, 6684]
+		//[1374, 5970,   730, 1951,  5909, 26692, 2012, 5361] even sampleOffset = 1
+		//[1374, 5909,   730, 2012,  5970, 26692, 1951, 5360] reversed even
 		//[5327, 1956, 26612, 6050,  2017,   725, 5989, 1323] sampleOffset = 0
-
-
+		
 	}
 
 }
