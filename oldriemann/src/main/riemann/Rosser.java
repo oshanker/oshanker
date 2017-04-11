8b42984e0fdf1d2310035abed9898e11e78c06be
oshanker <oshanker@hotmail.com>
2017-04-10 18:20:38 -0700

    debugging the gram block classification

--- oldriemann/src/main/riemann/Rosser.java
+++ oldriemann/src/main/riemann/Rosser.java
@@ -9,6 +9,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.TreeMap;
 import java.util.TreeSet;
@@ -20,6 +21,7 @@ import java.util.TreeSet;
 public class Rosser {
 	static boolean hiary = false;
 	static HashMap<String, Integer> rosser = new HashMap<>();
+	private static int maxS = 10;
 	static class ZeroInfo{
 		String zeroInput;
 		int countZeros;
@@ -43,9 +45,10 @@ public class Rosser {
 		}
 	}
 	
-	public static ZeroInfo readZeros(  double upperLimit, PrintStream out, BufferedReader zeroIn, String input)
+	public static ZeroInfo readZeros(  double upperLimit, PrintStream out, 
+			BufferedReader zeroIn, String input, int S)
 			throws FileNotFoundException, IOException {
-		int countZeros = 0;
+		ArrayList<Double> countZeros = new ArrayList<>();
 		if(input == null){
 			input = zeroIn.readLine();
 		}
@@ -55,7 +58,8 @@ public class Rosser {
 		double zero = 0;
 		while (zero < upperLimit ) {
 			if(input == null || input.trim().length()==0){
-				break;
+				System.out.println("done");
+				return null;
 			}
 			input = input.trim();
 			if(hiary){
@@ -71,19 +75,20 @@ public class Rosser {
 				break;
 			}
 			print( out, zero + ",");
-			countZeros++;
+			countZeros.add(zero);
 			input = zeroIn.readLine();
 		}
-		println(out, Integer.toString(countZeros));
-		return new ZeroInfo(input,countZeros);
+		if(countZeros.size()>16){
+			System.out.println("really? upperLimit " + upperLimit + " " + countZeros);
+		}
+		println(out, Integer.toString(countZeros.size()));
+		return new ZeroInfo(input,countZeros.size());
 	}
 	
 	public static void readItems( int N, PrintStream out)
 			throws FileNotFoundException, IOException {
 		String zerosFile =hiary?"/Users/oshanker/Google Drive/Documents/Riemann/riemann/1e12.zeros.1001_10001002.txt":"data/zerosE12.csv";
 		//assuming that we start at a good regular odd Gram Point
-//		BufferedReader zeroIn = new BufferedReader(
-//				new FileReader("/Users/oshanker/Google Drive/Documents/Riemann/riemann/1e12.zeros.1001_10001002.txt"));
 		BufferedReader zeroIn = new BufferedReader(
 				new FileReader(zerosFile));
 		int count = 0;
@@ -95,27 +100,26 @@ public class Rosser {
 		boolean inGramBlock = false;
 		String interval = null;
 		int signumGram = hiary?1:-1;
-		if(hiary){
-			//baseLimit += gramIncr; 
-			good = true;
-			println(out, "n-3945951431270L,good");
-		} else {
-			println(out, "n-3945951431270L,good");
-		}
+		int goodCount = 0, badCount = 0;
+		println(out, "n-3945951431270L,good, S");
+		int S = 0;//starting at regular Gram Point
 		while (count < N  ) {
 			int n = count + (hiary?2:1);
 			double upperLimit = baseLimit + (n-1)* gramIncr;
 			if((n%2 == 1 && signumGram <= 0) || (n%2 == 0 && signumGram > 0)){
-				print(out, n + ",1" );
+				print(out, n + ",1, " + S);
 				good = true;
+				goodCount++;
 			} else {
-				print(out, n + ",0" );
+				print(out, n + ",0, " + S);
 				good = false;
+				badCount++;
 			}
 			if(inGramBlock){
 				interval += zeroInput.countZeros;
 			}
 			if(!(good ^ oldGood)){
+				//both good or both bad
 				println(out, "" );
 			} else {
 				//transition
@@ -132,9 +136,16 @@ public class Rosser {
 				}
 				inGramBlock = !inGramBlock;
 			}
-			zeroInput = readZeros(upperLimit , out, zeroIn, zeroInput.zeroInput);
+			zeroInput = readZeros(upperLimit , out, zeroIn, zeroInput.zeroInput, S);
+			if (count==N-1) {
+				System.out.println("final n " + n + " type " + good + " signumGram " + signumGram);
+			}
 			if (zeroInput==null) {
 				break;
+			}
+			S += zeroInput.countZeros - 1;
+			if(Math.abs(S) > maxS ){
+				System.out.println("S " + S + ", n " + n + ", zeroInput.countZeros " + zeroInput.countZeros );
 			}
 			if(zeroInput.countZeros%2 == 1){
 				signumGram = signumGram==-1?1:-1;
@@ -142,6 +153,8 @@ public class Rosser {
 			oldGood = good;
 			count++;
 		}
+		double ratio = ((double)badCount)/(goodCount+badCount);
+		System.out.println("goodCount " + goodCount + " badCount " + badCount + " " + ratio);
 		zeroIn.close();
 	}
 
@@ -178,7 +191,7 @@ public class Rosser {
 			if(idx < stats.length){
 				stats[idx].add(key + " : " + rosser.get(key));
 			} else {
-				System.out.println("* " + key.length());
+				System.out.println("* " + key.length() + " " + key + " " + rosser.get(key));
 			}
 		}
 		for (int i = 0; i < stats.length; i++) {
