54280d9225bb86eb6ca3a9fa05e5f4fefa13b0df
Shanker <oshanker@hotmail.com>
2018-02-04 10:17:31 -0800

    even-odd

--- R/histExample.R
+++ R/histExample.R
@@ -10,3 +10,9 @@ even<-y[y%%2==0]
 print(summary(x[even]))
 odd<-y[y%%2==1]
 print(summary(x[odd]))
+cross=x[odd]*x[even]
+print(summary(cross))
+b=seq(-100,25,5)
+b=c(-4000,b,12000)
+h=hist(cross,breaks=b,xlim=c(-100,25),freq=TRUE)
+
