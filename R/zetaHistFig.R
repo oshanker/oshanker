a20736195a5727259983284fb2af170249ddd566
Shanker <oshanker@hotmail.com>
2018-06-24 14:20:43 -0700

    qqplot

--- R/zetaHistFig.R
+++ R/zetaHistFig.R
@@ -2,8 +2,12 @@ df <- read.csv("../oldriemann/out/gzetaE12/gzeta6.csv", header = FALSE);
 i = 10;
 	x<-as.matrix(df[i]);
 	x<-as.vector(x);
+qqnorm(x, main = paste("Normal Q-Q Plot for Z(t) at ", expression(phi),
+   " = 3", expression(pi), "/2"),
+       xlab = "Theoretical Quantiles", ylab = "Sample Quantiles"); qqline(x, col = 2)
+       
 b=seq(-10,10,0.5)
 yy = range(x)
-b=c(yy[1],b,yy[2])
-h=hist(x,breaks=b,xlim=c(-10,10),xlab='Z',
-main = paste("Histogram of Z for 3" , expression(pi), "/2"))
+#b=c(yy[1],b,yy[2])
+#h=hist(x,breaks=b,xlim=c(-10,10),xlab='Z',
+# main = paste("Histogram of Z for 3" , expression(pi), "/2"))
