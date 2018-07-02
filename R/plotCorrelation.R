4b792a4c69c7c71319d8a382ef2405546832d93e
Shanker <oshanker@hotmail.com>
2018-07-02 13:17:30 -0700

    correlations plot

--- R/plotCorrelation.R
+++ R/plotCorrelation.R
@@ -1,5 +1,6 @@
 df <- read.csv("../oldriemann/out/gzetaCorrelation/gzeta8.csv", header = FALSE);
 ls(df)
-correlation = as.vector(df[9])
+i = c(1,9)
+correlation = as.vector(df[i])
 xx = seq(1,16) 
 matplot(xx,correlation,type = "l")
