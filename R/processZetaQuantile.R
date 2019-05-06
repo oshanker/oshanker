04f51d2fc7827e610129429220bb180aef122852
Oruganti Shanker <shankero@amazon.com>
2019-05-05 18:11:47 -0700

    print to csv

--- R/processZetaQuantile.R
+++ R/processZetaQuantile.R
@@ -1,12 +1,16 @@
 basedir<-"../oldriemann/out/gzetaE28"
-outtable <- read.table(paste0(basedir,"/percentile.txt"), header = FALSE);
+intable <- read.table(paste0(basedir,"/percentile.txt"), header = FALSE);
+outtable<-matrix(nrow =  2, ncol = 12, byrow = TRUE)
 meanValues<- c(1:12)
 slopes<- c(1:12)
 for (i in 2:12) {
-	x<-as.matrix(outtable[i]);
+	x<-as.matrix(intable[i]);
 	x<-as.vector(x);
 	meanValues[i]=mean(x) 
-	slopes[i]=(x[1]-x[13])/4
+	slopes[i]=(x[4]-x[10])/2.828
 }
-print(meanValues)
-print(slopes)
+outtable[1,1:length(meanValues)]<-meanValues
+outtable[2,1:length(slopes)]<-slopes
+write.csv(outtable,file=paste0(basedir,"/quantileSlopes.csv"))
+mytest=read.csv(paste0(basedir,"/quantileSlopes.csv"))
+print(mytest)
