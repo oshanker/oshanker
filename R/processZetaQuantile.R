e9d950fc4755e3ff2d1b3475a256781ff9efd86d
Oruganti Shanker <shankero@amazon.com>
2019-05-09 21:45:18 -0700

    mean E12 exactgit statusgit status

--- R/processZetaQuantile.R
+++ R/processZetaQuantile.R
@@ -5,13 +5,17 @@ slopes<- c(1:13)
 for (i in 2:13) {
 	x<-as.matrix(intable[i]);
 	x<-as.vector(x);
-	print(x)
-	print(paste('is',i))
+#	print(x)
+#	print(paste('is',i))
 	meanValues[i]=mean(x) 
 	slopes[i]=(x[3]-x[5])/2
 }
     conv = format( meanValues, scientific = FALSE, drop0trailing = TRUE, digits = 4, width =8)
-#    conv = as.numeric(conv)
     print(conv, quote = FALSE)
 #print(meanValues)
 print(slopes)
+
+ outtable<-matrix(nrow=2,ncol=length(meanValues))
+ outtable[1,1:dim(outtable)[2]]<-meanValues
+ outtable[2,1:dim(outtable)[2]]<-slopes
+ write.csv(outtable,file=paste0(basedir,"/quantileSlopes_calc.csv"), row.names = FALSE)
