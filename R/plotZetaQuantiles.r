251ba7ec2d4d690d35394290f3a0fcd1a206dc3c
Oruganti Shanker <shankero@amazon.com>
2019-05-11 13:31:24 -0700

    plotZetaQuantiles.r

--- R/plotZetaQuantiles.r
+++ R/plotZetaQuantiles.r
@@ -5,12 +5,12 @@ infile1<-"/quantileFittedE28.csv"
 intable <- read.csv(paste0(basedir,infile),header=FALSE);
 	x<-as.matrix(intable[1,3:13]);
 	x<-as.vector(x);
-	print(dim(y))
 
 	y0<-as.matrix(intable[2,3:13]);
 	y0<-as.vector(y0);
 	
 y<-matrix(nrow=length(x),ncol=2)
+	print(dim(y))
 	y[1:length(x),1]<-y0
 
 intable1 <- read.csv(paste0(basedir1,infile1),header=FALSE);
@@ -28,3 +28,5 @@ matplot(x, y, type = "l", xaxt = "n",#
         xlab = expression("Quantile ")#
         )#
 axis(1)
+text(0.9,3.9, "E28")
+text(0.9,2.8, "E12")
