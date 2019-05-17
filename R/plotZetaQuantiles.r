7111573002deb3e90c2a4dd4e3104c0a7e5d7aa1
Oruganti Shanker <shankero@amazon.com>
2019-05-16 23:20:17 -0700

    fit to params

--- R/plotZetaQuantiles.r
+++ R/plotZetaQuantiles.r
@@ -1,9 +1,9 @@
 basedir<-"../oldriemann/data/gzetaE12"
 infile<-"/quantileFitted_calc6.csv"
-basedir1<-"../oldriemann/out/gzetaE28"
+basedir1<-"../oldriemann/data/gzetaE28"
 infile1<-"/quantileFittedE28.csv"
 ranges=3:13
-plotindex<-3
+plotindex<-2
 intable <- read.csv(paste0(basedir,infile),header=FALSE);
 	x<-as.matrix(intable[1,ranges]);
 	x<-as.vector(x);
@@ -11,7 +11,7 @@ intable <- read.csv(paste0(basedir,infile),header=FALSE);
 	y0<-as.matrix(intable[plotindex,ranges]);
 	y0<-as.vector(y0);
 	
-y<-matrix(nrow=length(x),ncol=2)
+y<-matrix(nrow=length(x),ncol=4)
 	print(dim(y))
 	y[1:length(x),1]<-y0
 
@@ -24,12 +24,30 @@ intable1 <- read.csv(paste0(basedir1,infile1),header=FALSE);
 print(x)
 print(y)
 
-matplot(x, y, type = "l", xaxt = "n",#
+f=0.1
+den=0.25-f^2
+a=(y[7,1:2]-y[5,1:2])*den/(2*f)
+
+f=x[11]-0.5
+den=0.25-f^2
+b=(y[11,1:2]*den-a*f)/(f*f*f)
+
+print(paste(c("a12","a28"),a))
+print(paste(c("b12","b28"),b))
+
+for (i in 1:length(x)) {
+   xx=x[i]-0.5
+   den=0.25-xx^2
+   y[i,3:4]=(a*xx+b*xx*xx*xx)/den
+}
+
+matplot(x, y, type = "b", xaxt = "n",#
         main = "Quantile",#
+        pch = "1*ab", 
         ylab = expression("mean Z"), # only 1st is taken#
-        xlab = expression("Quantile ")#
+        xlab = expression("f")#
         )#
 axis(1)
-text(0.9,3.9, "E28")
+text(0.84,3.9, "E28")
 text(0.9,2.8, "E12")
 grid()
