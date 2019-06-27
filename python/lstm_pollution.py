ec32220e3dea6fce127fcebced11518aa1dee748
Oruganti Shanker <shankero@amazon.com>
2019-06-27 07:49:58 -0700

    include or exclude actual param

--- python/lstm_pollution.py
+++ python/lstm_pollution.py
@@ -61,8 +61,10 @@ n_train_hours = 365 * 24
 train = values[:n_train_hours, :]
 test = values[n_train_hours:, :]
 # split into input and outputs
-train_X, train_y = train[:, :-1], train[:, -1]
-test_X, test_y = test[:, :-1], test[:, -1]
+#drop pollution as predictor if start = 1
+start = 0
+train_X, train_y = train[:, start:-1], train[:, -1]
+test_X, test_y = test[:, start:-1], test[:, -1]
 # reshape input to be 3D [samples, timesteps, features]
 train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
 test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
@@ -85,35 +87,35 @@ def plot_history():
 	pyplot.show()
  
 # make a prediction
+plotsize = 10000
 yhat = model.predict(test_X)
 test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
 # invert scaling for forecast
-inv_yhat = concatenate((yhat, test_X[:, 1:]), axis=1)
+inv_yhat = concatenate((yhat, test_X[:, (1-start):]), axis=1)
 inv_yhat = scaler.inverse_transform(inv_yhat)
 inv_yhat = inv_yhat[:,0]
-print("forecast ", inv_yhat[0:5])
+forecast = inv_yhat[0:plotsize]
+#print("forecast ", inv_yhat[0:5])
 # invert scaling for actual
 test_y = test_y.reshape((len(test_y), 1))
-inv_y = concatenate((test_y, test_X[:, 1:]), axis=1)
+inv_y = concatenate((test_y, test_X[:, (1-start):]), axis=1)
 inv_y = scaler.inverse_transform(inv_y)
 inv_y = inv_y[:,0]
-actual = inv_y[0:5]
-print(type(actual))
-print("actual ", actual)
+actual = inv_y[0:plotsize]
+#print("actual ", actual)
 # calculate RMSE
 rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
-print('Test RMSE: %.3f' % rmse)
-joined = np.array([inv_yhat[0:5], actual])
+print('Test RMSE: %.3f' % rmse, ' actual mean %.3f' % inv_y.mean(), 
+   ' prediction mean %.3f' % inv_yhat.mean())
+joined = np.array([forecast, actual])
 joined = joined.transpose()
 df = pd.DataFrame(joined,  columns =["forecast", "actual"])
 values=df.values
 groups = [0, 1]
-i = 1
 # plot each column
 pyplot.figure()
 for group in groups:
-	pyplot.subplot(len(groups), 1, i)
-	pyplot.plot(values[:, group])
-	pyplot.title(df.columns[group], y=0.5, loc='right')
-	i += 1
+	pyplot.plot(values[:, group], label=df.columns[group])
+
+pyplot.legend()
 pyplot.show()
