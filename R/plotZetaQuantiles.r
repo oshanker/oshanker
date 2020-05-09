7caa502b1b87a82fea380afa02ae25714f94faeb
Oruganti Shanker <shankero@amazon.com>
2020-05-09 09:27:07 -0700

    quantiles linear fit

--- R/plotZetaQuantiles.r
+++ R/plotZetaQuantiles.r
@@ -1,27 +1,26 @@
 basedir<-c("../oldriemann/data/gzetaE12","../oldriemann/data/gzetaE28")
 infile<-c("/quantileFitted_calc6.csv", "/quantileFittedE28.csv")
-#basedir1<-"../oldriemann/data/gzetaE28"
-#infile1<-"/quantileFittedE28.csv"
 ranges=3:13
-plotindex<-3
+# cover f from 0.1 to 0.9
+plotindex <- 2
+# 3 : slope; 2 : intercept
 y<-matrix(nrow=length(ranges),ncol=4)
-	print(dim(y))
+print(dim(y))
 
 intable <- read.csv(paste0(basedir[1],infile[1]),header=FALSE);
-	f<-as.matrix(intable[1,ranges]);
-	f<-as.vector(f);
+f<-as.matrix(intable[1,ranges]);
+f<-as.vector(f);
 
+y0<-as.matrix(intable[plotindex,ranges]);
+y0<-as.vector(y0);
 
-	y0<-as.matrix(intable[plotindex,ranges]);
-	y0<-as.vector(y0);
-	
-	y[1:length(f),1]<-y0
+y[1:length(f),1]<-y0
 
 intable1 <- read.csv(paste0(basedir[2],infile[2]),header=FALSE);
-	y1<-as.matrix(intable1[plotindex,ranges]);
-	y1<-as.vector(y1);
-	
-	y[1:length(f),2]<-y1
+y1<-as.matrix(intable1[plotindex,ranges]);
+y1<-as.vector(y1);
+
+y[1:length(f),2]<-y1
 
 print(f)
 print(y)
@@ -59,6 +58,7 @@ if(plotindex==2){
 	}
 	rootcheck=(a[1]*poles2[2]-a[2]*poles2[1])/(a[1]-a[2])
 	print(paste('rootcheck', sqrt(rootcheck) ) )
+	label = 'intercept'
 } else if(plotindex==3){
 	legendposition="center"
 	poles2=c(0.41, 0.45)
@@ -71,7 +71,7 @@ if(plotindex==2){
 		fit0=coef(lm.fit2)
 		print(fit0)
 		y[1:length(xxvec),i+2]=(fit0[1] + fit0[2]*xx2)/denvec
-
+		label = 'slope'
 	}
 	
 }
@@ -79,7 +79,7 @@ symbols="12a*"
 matplot(f, y, type = "b", xaxt = "n",#
         main = "Quantile",#
         pch = symbols, 
-        ylab = expression("mean Z"), # only 1st is taken#
+        ylab = label, # only 1st is taken#
         xlab = expression("f")#
         )#
 axis(1)
