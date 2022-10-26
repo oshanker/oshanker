36faed24daa70e5121501a08266267b4e9e67674
oshanker <oshanker@hotmail.com>
2022-10-26 00:12:57 -0700

    fix (scale)

--- python/fix_pollution.py
+++ python/fix_pollution.py
@@ -1,19 +1,33 @@
+import sys
 from pandas import read_csv
 from datetime import datetime
+
+
 # load data
 def parse(x):
 	return datetime.strptime(x, '%Y %m %d %H')
-dataset = read_csv('data/pollution_raw.csv',  parse_dates = [['year', 'month', 'day', 'hour']], index_col=0, date_parser=parse)
-dataset.drop('No', axis=1, inplace=True)
-# manually specify column names
-dataset.columns = ['pollution', 'dew', 'temp', 'press', 'wnd_dir', 'wnd_spd', 'snow', 'rain']
-dataset.index.name = 'date'
-# mark all NA values with 0
-dataset['pollution'].fillna(0, inplace=True)
-# drop the first 24 hours
-dataset = dataset[24:]
-# summarize first 5 rows
-print('head', dataset.head(5))
-print('tail', dataset.tail(5))
-# save to file
-dataset.to_csv('data/pollution.csv')
+
+def main():
+    print("-------")
+    print(sys.argv)
+    # ~/junk/oshanker/oshanker/python/data/
+    file_name =  sys.argv[1]     
+    dataset = read_csv(file_name+'pollution_raw.csv',  
+        parse_dates = [['year', 'month', 'day', 'hour']], index_col=0, date_parser=parse)
+    dataset.drop('No', axis=1, inplace=True)
+    # manually specify column names
+    dataset.columns = ['pollution', 'dew', 'temp', 'press', 'wnd_dir', 'wnd_spd', 'snow', 'rain']
+    dataset.index.name = 'date'
+    # mark all NA values with 0
+    dataset['pollution'].fillna(0, inplace=True)
+    # drop the first 24 hours
+    dataset = dataset[24:]
+    # summarize first 5 rows
+    print('head', dataset.head(5))
+    print('tail', dataset.tail(5))
+    # save to file
+    dataset.to_csv(file_name + 'pollution.csv')
+    
+
+if __name__   == '__main__':
+     main()
--- python/plot_pollution.py
+++ python/plot_pollution.py
@@ -1,16 +1,29 @@
+import sys
 from pandas import read_csv
 from matplotlib import pyplot
-# load dataset
-dataset = read_csv('data/pollution.csv', header=0, index_col=0)
-values = dataset.values
-# specify columns to plot
-groups = [0, 1, 2, 3, 5, 6, 7]
-i = 1
-# plot each column
-pyplot.figure()
-for group in groups:
-	pyplot.subplot(len(groups), 1, i)
-	pyplot.plot(values[:, group])
-	pyplot.title(dataset.columns[group], y=0.5, loc='right')
-	i += 1
-pyplot.show()
+
+
+def main():
+    # load dataset
+    print("-------")
+    print(sys.argv)
+    # ~/junk/oshanker/oshanker/python/data/pollution.csv
+    file_name =  sys.argv[1]     
+    dataset = read_csv(file_name, header=0, index_col=0)
+    print(dataset)
+    values = dataset.values
+    # specify columns to plot
+    groups = [0, 1, 2, 3, 5, 6, 7]
+    i = 1
+    # plot each column
+    pyplot.figure()
+    for group in groups:
+    	pyplot.subplot(len(groups), 1, i)
+    	pyplot.plot(values[:, group])
+    	pyplot.title(dataset.columns[group], y=0.5, loc='right')
+    	i += 1
+    pyplot.show()
+    
+
+if __name__   == '__main__':
+     main()
