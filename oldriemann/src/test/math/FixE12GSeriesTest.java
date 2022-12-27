4b772cccc26df891ed77d55c611069c0b244213b
oshanker <oshanker@hotmail.com>
2022-12-26 20:56:38 -0800

    use list constructor

--- oldriemann/src/test/math/FixE12GSeriesTest.java
+++ oldriemann/src/test/math/FixE12GSeriesTest.java
@@ -2,8 +2,12 @@ package math;
 
 import org.junit.Assert;
 import org.junit.Test;
+import riemann.Interpolate;
 
+import java.util.Arrays;
 import java.util.LinkedList;
+
+import static math.FixE12GSeries.TEST_VALES;
 
 public class FixE12GSeriesTest  {
     public void testTestIncrementGValuesAtIndices() {
@@ -32,6 +36,25 @@ public class FixE12GSeriesTest  {
     }
     
     @Test
+    public void testTestChangeToZetaAndDerNoMax1() {
+        LinkedList<double[]> zeroInfo = new LinkedList<>();
+        for (int i = 0; i < TEST_VALES.length; i++) {
+            zeroInfo.add(TEST_VALES[i]);
+        }
+    
+        GSeries gAtBeta = Interpolate.readGSeries();
+    
+        FixE12GSeries fixE12GSeries = new FixE12GSeries(zeroInfo, -1000, gAtBeta);
+        double[][] ret = fixE12GSeries.testChangeToZetaAndDerNoMax(gAtBeta);
+        double[] neededZetaIncrement = ret[1];
+        double[] actualIncrementInValues = ret[0];
+        for (int i = 0; i < actualIncrementInValues.length; i++) {
+            Assert.assertEquals(actualIncrementInValues[i], neededZetaIncrement[i], 5.0E-6) ;
+        }
+        //System.out.println(" " + Arrays.toString(actualIncrementInValues));
+    }
+    
+    @Test
     public void testTestChangeToZetaAndDerNoMax() {
         FixE12GSeries fixE12GSeries = new FixE12GSeries();
         double[][] ret = fixE12GSeries.testChangeToZetaAndDerNoMax(fixE12GSeries.gAtBeta);
@@ -40,5 +63,6 @@ public class FixE12GSeriesTest  {
         for (int i = 0; i < actualIncrementInValues.length; i++) {
             Assert.assertEquals(actualIncrementInValues[i], neededZetaIncrement[i], 5.0E-6) ;
         }
+        //System.out.println(" " + Arrays.toString(actualIncrementInValues));
     }
 }
