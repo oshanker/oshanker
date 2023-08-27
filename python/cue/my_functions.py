46d73ffaff0a99378a05bcc6b11476c973992642
oshanker <oshanker@hotmail.com>
2023-08-26 19:23:16 -0700

    save changes

--- python/cue/my_functions.py
+++ python/cue/my_functions.py
@@ -9,6 +9,7 @@ import math
 import numpy as np
 from numpy.linalg import qr
 import cmath
+from sklearn.linear_model import LinearRegression
 
 def sample(n):
     z = (np.random.randn(n,n) + 1j*np.random.randn(n,n))/ np.lib.scimath.sqrt(2.0)
@@ -32,6 +33,19 @@ def sample(n):
         
     angles = np.sort( angles)
     return angles
+
+def fit(values, phi_values):
+    x = []
+    for phi in phi_values:
+        x.append([math.cos(math.pi*phi/180), math.cos(math.pi*phi/90)])
+        #x.append([math.cos(math.pi*phi/180)])
+    x = np.array(x)
+    reg = LinearRegression().fit(x, values)
+    r2 = reg.score(x, values)
+    #
+    coeff = reg.coef_
+    intercept = reg.intercept_
+    return [intercept, coeff[0], coeff[1], r2]
 
 
 def eval_direct(theta, root_angles):
