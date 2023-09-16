d8ca766ec124db952f452dee0c255c4d05bb2a70
oshanker <oshanker@hotmail.com>
2023-09-16 15:46:04 -0700

    extend fit range?

--- python/cue/plot_dist.py
+++ python/cue/plot_dist.py
@@ -10,8 +10,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import my_functions
 
-b_fit = False
-a_fit = True
+b_fit = True
+#b_fit = False
+#a_fit = True
+a_fit = False
     
 def b_der_exp_gauss(x, b, p, lam, sigma):
     ret = b*my_functions.der_exp_gauss(x, p, lam, sigma)
@@ -66,14 +68,24 @@ def main():
     # do_plot_with_data(b_der_exp_gauss, popt, xdata, b_data, 'ydata')
     
     if b_fit:
-     bounds=([-2.005, 0.2, 0.6, 7.5], [-1.999, 0.3, 0.8, 8.5])
+     bounds=([-2.005, 0.1, 0.1, 8.5], [-1.999, 0.5, 0.7, 10.5])
      popt = my_functions.do_fit(b_der_exp_gauss, xdata, b_data, bounds=bounds) #0.9
+     p = popt[1]
+     lam = popt[2]
+     sigma = popt[3]
      print('param', popt)
+     print('2/(lam*lam) ', 2/(lam*lam) , " + sigma*sigma ", sigma*sigma
+          , ' = ', p*2/(lam*lam) + (1-p)*sigma*sigma)
      
     if a_fit:
-       bounds=([0.4, 0.1, 9.4], [0.7, 1.5, 9.5])
+       bounds=([0.1, 0.1, 9.0], [0.7, 1.5, 9.5])
        popt = my_functions.do_fit(my_functions.exp_gauss, xdata, ydata, bounds=bounds) #0.9
+       p = popt[0]
+       lam = popt[1]
+       sigma = popt[2]
        print('param', popt)
+       print('2/(lam*lam) ', 2/(lam*lam) , " + sigma*sigma ", sigma*sigma
+          , ' = ', p*2/(lam*lam) + (1-p)*sigma*sigma)
     #param [             0.61859396  1.42590918  9.4       ]
     #param [-1.997       0.22968019  0.69744049  7.73814023]
     
