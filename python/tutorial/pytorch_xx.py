e7746a83e7e9b976b28b0238c2a20bdb626de7be
oshanker <oshanker@hotmail.com>
2024-05-25 12:16:20 -0700

    torch ref

--- python/tutorial/pytorch_xx.py
+++ python/tutorial/pytorch_xx.py
@@ -24,4 +24,5 @@ y = np.array([2, 3, 4])
 print(np.dot(x, y))
 
 # https://statistics.berkeley.edu/computing/faqs/git-auth
+# https://towardsdatascience.com/a-complete-guide-to-write-your-own-transformers-29e23f371ddd
 # https://towardsdatascience.com/build-your-own-transformer-from-scratch-using-pytorch-84c850470dcb
