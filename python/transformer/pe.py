f1bf9394e47670fbdef75b7701a1445131ac98ad
oshanker <oshanker@hotmail.com>
2024-05-26 21:37:52 -0700

    Encoder

--- python/transformer/pe.py
+++ python/transformer/pe.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import math
+from mha import MultiHeadAttention
 
 
 # Taken from https://pytorch.org/tutorials/beginner/transformer_tutorial.html#define-the-model
@@ -28,10 +29,70 @@ class PositionalEncoding(nn.Module):
         x = x + self.pe[:, :x.size(1), :]
         return x
 
+class PositionWiseFeedForward(nn.Module):
+    def __init__(self, d_model: int, d_ff: int):
+        super(PositionWiseFeedForward, self).__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_model)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
+
+class EncoderBlock(nn.Module):
+    def __init__(self, n_dim: int, dropout: float, n_heads: int):
+        super(EncoderBlock, self).__init__()
+        self.mha = MultiHeadAttention(hidden_dim=n_dim, num_heads=n_heads)
+        self.norm1 = nn.LayerNorm(n_dim)
+        self.ff = PositionWiseFeedForward(n_dim, n_dim)
+        self.norm2 = nn.LayerNorm(n_dim)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x, src_padding_mask=None):
+        assert x.ndim==3, "Expected input to be 3-dim, got {}".format(x.ndim)
+        att_output = self.mha(x, x, x, key_padding_mask=src_padding_mask)
+        x = x + self.dropout(self.norm1(att_output))
+        
+        ff_output = self.ff(x)
+        output = x + self.norm2(ff_output)
+       
+        return output
+
+class Encoder(nn.Module):
+    def __init__(
+            self, 
+            vocab_size: int, 
+            n_dim: int, 
+            dropout: float, 
+            n_encoder_blocks: int,
+            n_heads: int):
+        
+        super(Encoder, self).__init__()
+        self.n_dim = n_dim
+
+        self.embedding = nn.Embedding(
+            num_embeddings=vocab_size, 
+            embedding_dim=n_dim
+        )
+        self.positional_encoding = PositionalEncoding(
+            d_model=n_dim, 
+            dropout=dropout
+        )    
+        self.encoder_blocks = nn.ModuleList([
+            EncoderBlock(n_dim, dropout, n_heads) for _ in range(n_encoder_blocks)
+        ])
+        
+        
+    def forward(self, x, padding_mask=None):
+        x = self.embedding(x) * math.sqrt(self.n_dim)
+        x = self.positional_encoding(x)
+        for block in self.encoder_blocks:
+            x = block(x=x, src_padding_mask=padding_mask)
+        return x
 
 def runperson():
-	p1 = PositionalEncoding(4)
-	print("hidden_dim", p1)
+	p1 = Encoder(12, 4, 0.1, 5, 4)
+	print("class", p1)
 	
 if __name__ == "__main__":
     runperson()
