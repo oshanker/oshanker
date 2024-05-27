02adaf64993e4b8526b48ab72cf75d6718322133
oshanker <oshanker@hotmail.com>
2024-05-26 21:13:02 -0700

    pe

---
+++ python/transformer/pe.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import math
+
+
+# Taken from https://pytorch.org/tutorials/beginner/transformer_tutorial.html#define-the-model
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        """
+        Arguments:
+            x: Tensor, shape ``[batch_size, seq_len, embedding_dim]``
+        """
+        x = x + self.pe[:, :x.size(1), :]
+        return x
+
+
+def runperson():
+	p1 = PositionalEncoding(4)
+	print("hidden_dim", p1)
+	
+if __name__ == "__main__":
+    runperson()
