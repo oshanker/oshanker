7a3e0301c56a6865310ace13ff486c03eac1249d
oshanker <oshanker@hotmail.com>
2024-05-26 21:55:03 -0700

    transformer

--- python/transformer/pe.py
+++ python/transformer/pe.py
@@ -90,8 +90,81 @@ class Encoder(nn.Module):
             x = block(x=x, src_padding_mask=padding_mask)
         return x
 
+class DecoderBlock(nn.Module):
+    def __init__(self, n_dim: int, dropout: float, n_heads: int):
+        super(DecoderBlock, self).__init__()
+        
+        # The first Multi-Head Attention has a mask to avoid looking at the future
+        self.self_attention = MultiHeadAttention(hidden_dim=n_dim, num_heads=n_heads)
+        self.norm1 = nn.LayerNorm(n_dim)
+        
+        # The second Multi-Head Attention will take inputs from the encoder as key/value inputs
+        self.cross_attention = MultiHeadAttention(hidden_dim=n_dim, num_heads=n_heads)
+        self.norm2 = nn.LayerNorm(n_dim)
+        
+        self.ff = PositionWiseFeedForward(n_dim, n_dim)
+        self.norm3 = nn.LayerNorm(n_dim)
+        # self.dropout = nn.Dropout(dropout)
+        
+        
+    def forward(self, tgt, memory, tgt_mask=None, tgt_padding_mask=None, memory_padding_mask=None):
+        
+        masked_att_output = self.self_attention(
+            q=tgt, k=tgt, v=tgt, attention_mask=tgt_mask, key_padding_mask=tgt_padding_mask)
+        x1 = tgt + self.norm1(masked_att_output)
+        
+        cross_att_output = self.cross_attention(
+            q=x1, k=memory, v=memory, attention_mask=None, key_padding_mask=memory_padding_mask)
+        x2 = x1 + self.norm2(cross_att_output)
+        
+        ff_output = self.ff(x2)
+        output = x2 + self.norm3(ff_output)
+
+        
+        return output
+
+class Decoder(nn.Module):
+    def __init__(
+        self, 
+        vocab_size: int, 
+        n_dim: int, 
+        dropout: float, 
+        n_decoder_blocks: int,
+        n_heads: int):
+        
+        super(Decoder, self).__init__()
+        
+        self.embedding = nn.Embedding(
+            num_embeddings=vocab_size, 
+            embedding_dim=n_dim,
+            padding_idx=0
+        )
+        self.positional_encoding = PositionalEncoding(
+            d_model=n_dim, 
+            dropout=dropout
+        )
+          
+        self.decoder_blocks = nn.ModuleList([
+            DecoderBlock(n_dim, dropout, n_heads) for _ in range(n_decoder_blocks)
+        ])
+        
+        
+    def forward(self, tgt, memory, tgt_mask=None, tgt_padding_mask=None, memory_padding_mask=None):
+        x = self.embedding(tgt)
+        x = self.positional_encoding(x)
+
+        for block in self.decoder_blocks:
+            x = block(
+                x, 
+                memory, 
+                tgt_mask=tgt_mask, 
+                tgt_padding_mask=tgt_padding_mask, 
+                memory_padding_mask=memory_padding_mask)
+        return x
+
+
 def runperson():
-	p1 = Encoder(12, 4, 0.1, 5, 4)
+	p1 = Decoder(12, 4, 0.1, 5, 4)
 	print("class", p1)
 	
 if __name__ == "__main__":
---
+++ python/transformer/transformer.py
@@ -0,0 +1,138 @@
+import torch
+import torch.nn as nn
+import math
+
+from pe import Encoder, Decoder
+#from .decoder import Decoder
+
+
+class Transformer(nn.Module):
+    def __init__(self, **kwargs):
+        super(Transformer, self).__init__()
+        
+        for k, v in kwargs.items():
+            print(f" * {k}={v}")
+        
+        self.vocab_size = kwargs.get('vocab_size')
+        self.model_dim = kwargs.get('model_dim')
+        self.dropout = kwargs.get('dropout')
+        self.n_encoder_layers = kwargs.get('n_encoder_layers')
+        self.n_decoder_layers = kwargs.get('n_decoder_layers')
+        self.n_heads = kwargs.get('n_heads')
+        self.batch_size = kwargs.get('batch_size')
+        self.PAD_IDX = kwargs.get('pad_idx', 0)
+
+        self.encoder = Encoder(
+            self.vocab_size, self.model_dim, self.dropout, self.n_encoder_layers, self.n_heads)
+        self.decoder = Decoder(
+            self.vocab_size, self.model_dim, self.dropout, self.n_decoder_layers, self.n_heads)
+        self.fc = nn.Linear(self.model_dim, self.vocab_size)
+        
+
+    @staticmethod    
+    def generate_square_subsequent_mask(size: int):
+            """Generate a triangular (size, size) mask. From PyTorch docs."""
+            mask = (1 - torch.triu(torch.ones(size, size), diagonal=1)).bool()
+            mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+            return mask
+
+
+    def encode(
+            self, 
+            x: torch.Tensor, 
+        ) -> torch.Tensor:
+        """
+        Input
+            x: (B, S) with elements in (0, C) where C is num_classes
+        Output
+            (B, S, E) embedding
+        """
+
+        mask = (x == self.PAD_IDX).float()
+        encoder_padding_mask = mask.masked_fill(mask == 1, float('-inf'))
+        
+        # (B, S, E)
+        encoder_output = self.encoder(
+            x, 
+            padding_mask=encoder_padding_mask
+        )  
+        
+        return encoder_output, encoder_padding_mask
+    
+    
+    def decode(
+            self, 
+            tgt: torch.Tensor, 
+            memory: torch.Tensor, 
+            memory_padding_mask=None
+        ) -> torch.Tensor:
+        """
+        B = Batch size
+        S = Source sequence length
+        L = Target sequence length
+        E = Model dimension
+        
+        Input
+            encoded_x: (B, S, E)
+            y: (B, L) with elements in (0, C) where C is num_classes
+        Output
+            (B, L, C) logits
+        """
+        
+        mask = (tgt == self.PAD_IDX).float()
+        tgt_padding_mask = mask.masked_fill(mask == 1, float('-inf'))
+
+        decoder_output = self.decoder(
+            tgt=tgt, 
+            memory=memory, 
+            tgt_mask=self.generate_square_subsequent_mask(tgt.size(1)), 
+            tgt_padding_mask=tgt_padding_mask, 
+            memory_padding_mask=memory_padding_mask,
+        )  
+        output = self.fc(decoder_output)  # shape (B, L, C)
+        return output
+
+        
+        
+    def forward(
+            self, 
+            x: torch.Tensor, 
+            y: torch.Tensor, 
+        ) -> torch.Tensor:
+        """
+        Input
+            x: (B, Sx) with elements in (0, C) where C is num_classes
+            y: (B, Sy) with elements in (0, C) where C is num_classes
+        Output
+            (B, L, C) logits
+        """
+        
+        # Encoder output shape (B, S, E)
+        encoder_output, encoder_padding_mask = self.encode(x)  
+
+        # Decoder output shape (B, L, C)
+        decoder_output = self.decode(
+            tgt=y, 
+            memory=encoder_output, 
+            memory_padding_mask=encoder_padding_mask
+        )  
+        
+        return decoder_output
+
+def runperson():
+	# Model hyperparameters
+	args = {
+		'vocab_size': 128,
+		'model_dim': 128,
+		'dropout': 0.1,
+		'n_encoder_layers': 1,
+		'n_decoder_layers': 1,
+		'n_heads': 4
+	}
+
+	# Define model here
+	p1 = Transformer(**args)
+	print("class", p1)
+	
+if __name__ == "__main__":
+    runperson()
