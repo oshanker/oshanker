2a2e7cb04b1028c3289cddf4dffc9e366734d0bd
oshanker <oshanker@hotmail.com>
2024-05-26 19:47:48 -0700

    padding

--- python/transformer/base_transformer_shanker/data/stringdata.py
+++ python/transformer/base_transformer_shanker/data/stringdata.py
@@ -6,23 +6,23 @@ from torch.utils.data import Dataset
 np.random.seed(0)
 
 def generate_random_string():
-    #len = np.random.randint(10, 20)
-    len = 12
+    len = np.random.randint(10, 20)
+    #len = 12
     return "".join([chr(x) for x in np.random.randint(97, 97+26, len)])
 
-class ReverseDataset(Dataset):
+class GenerateDataset(Dataset):
 
     PAD_IDX = 0
     SOS_IDX = 1
     EOS_IDX = 2
     
-    def __init__(self, n_samples, pad_idx=PAD_IDX, sos_idx=SOS_IDX, eos_idx=EOS_IDX):
-        super(ReverseDataset, self).__init__()
+    def __init__(self, n_samples, pad_idx=PAD_IDX, sos_idx=SOS_IDX, eos_idx=EOS_IDX, reverseString=True):
+        super(GenerateDataset, self).__init__()
         self.pad_idx = pad_idx
         self.sos_idx = sos_idx
         self.eos_idx = eos_idx
         self.values = [generate_random_string() for _ in range(n_samples)]
-        self.labels = [x[::-1] for x in self.values]
+        self.labels = [x[::-1] for x in self.values] if reverseString else self.values
 
     def __len__(self):
         return len(self.values)  # number of samples in the dataset
--- python/transformer/runStringData.py
+++ python/transformer/runStringData.py
@@ -1,24 +1,41 @@
 import base_transformer_shanker.data
 from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
+from base_transformer_shanker.data import GenerateDataset as GD
 
-
-PAD_IDX = base_transformer_shanker.data.ReverseDataset.PAD_IDX
-SOS_IDX = base_transformer_shanker.data.ReverseDataset.SOS_IDX
-EOS_IDX = base_transformer_shanker.data.ReverseDataset.EOS_IDX
+PAD_IDX = GD.PAD_IDX
+SOS_IDX = GD.SOS_IDX
+EOS_IDX = GD.EOS_IDX
 
 def tokens_to_str(tokens):
 	return "".join([chr(x+94) for x in tokens])
 
+def collate_fn(batch):
+    """ 
+    This function pads inputs with PAD_IDX to have batches of equal length
+    """
+    src_batch, tgt_batch = [], []
+    for src_sample, tgt_sample in batch:
+        src_batch.append(src_sample)
+        tgt_batch.append(tgt_sample)
+
+    src_batch = pad_sequence(src_batch, padding_value=PAD_IDX, batch_first=True)
+    tgt_batch = pad_sequence(tgt_batch, padding_value=PAD_IDX, batch_first=True)
+    return src_batch, tgt_batch
+
 
 def runpersonMain():
-	train_iter = base_transformer_shanker.data.ReverseDataset(6, pad_idx=PAD_IDX, sos_idx=SOS_IDX, eos_idx=EOS_IDX)
-	dataloader_train = DataLoader(train_iter, batch_size=3)
+	train_iter = GD(6, pad_idx=PAD_IDX, sos_idx=SOS_IDX, eos_idx=EOS_IDX)
+	dataloader_train = DataLoader(train_iter, batch_size=3, collate_fn=collate_fn)
 	s, t = next(iter(dataloader_train))
 	print(s[:, ...])
 	print(t[:, ...])
 	print("s.size", s.size())
 	print("?", tokens_to_str(s[0, :]))
 	print("?", tokens_to_str(t[0, :]))
+	
+	for s, t in iter(dataloader_train):
+	     print(s.size())
 
 
 print(dir(base_transformer_shanker.data))
