cae6ed05136d43cca2ec032545f4488238ff975b
oshanker <oshanker@hotmail.com>
2024-06-05 23:17:39 -0700

    housekeeping

--- python/transformer/train.py
+++ python/transformer/train.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 from torch.utils.data import DataLoader
 from transformer import Transformer
 import base_transformer_shanker.functions as functions
-#from base_transformer_shanker.data.intervalsData import  IntervalsDataset 
+from base_transformer_shanker.data.intervalsData import  IntervalsDataset 
 from base_transformer_shanker.data.stringdata1 import GenerateNoMarkerDataset
 from base_transformer_shanker.data.fixedData import  FixedDataset 
 from base_transformer_shanker.constants import args 
@@ -124,41 +124,21 @@ def evaluate1(model, eval_iter, ignore_index, collate_fn):
 
     return losses / length, acc / length
 
-def evaluate2(model, dataloader, loss_fn, ignore_index):
+def evaluate2(model, dataloader):
     model.eval()
-    losses = 0
-    acc = 0
 
-    for x, y in tqdm(dataloader, position=0, leave=True):
-        print("x --> ", x.size())
-        for row in functions.iterate_rows(x):
-            print((row))
-        print("y (expected) --> ", y.size())
-            
-        
+    for x, y in dataloader:
         logits = model(x, y)
-        functions.plot_list(logits[0,0,:].detach().numpy())
         #print(logits[0,0,:].detach().numpy())
-        loss = loss_fn(logits.contiguous().view(-1, model.vocab_size), 
-                       y.contiguous().view(-1))
-        losses += loss.item()
-        
         preds = logits.argmax(dim=-1)
-        masked_pred = preds * (y != ignore_index) if ignore_index > -50 else preds
+        masked_pred = preds  
         accuracy = (masked_pred == y).float().mean()
-        
+        print('accuracy', accuracy.detach().numpy() )
         for step in range(0, y.size()[0]):
             actual = y[step, :].detach().numpy()-1
             mypred = masked_pred[step, :].detach().numpy() - 1
             print("actual    ", actual)
             print("prediction", mypred)
-
-        acc += accuracy.item()
-    
-    length = len(list(dataloader)) 
-    
-
-    return losses / length, acc / length
 
 def run(model, optimizer, dataloader_train, dataloader_val, ignore_index, title):
     loss_fn = torch.nn.CrossEntropyLoss()
@@ -237,7 +217,8 @@ def runTrain(train_iter, train_iter_1, eval_iter, eval_iter_1,
 #     torch.save(model.state_dict(), path)
 
 
-def runIntervalTrain(train_iter, train_iter_1, eval_iter, path, ignore_index: int = -100):
+def runIntervalTrain(train_iter, train_iter_1, eval_iter, eval_iter_1, 
+                     path, ignore_index: int = -100):
     # Define model here
     model = Transformer(**args)
 
@@ -245,6 +226,7 @@ def runIntervalTrain(train_iter, train_iter_1, eval_iter, path, ignore_index: in
     dataloader_train = DataLoader(train_iter, batch_size=256)
     dataloader_train_1 = DataLoader(train_iter_1, batch_size=256)
     dataloader_val = DataLoader(eval_iter, batch_size=256)
+    dataloader_val_1 = DataLoader(eval_iter_1, batch_size=256)
 
 
     # Initialize model parameters
@@ -252,76 +234,23 @@ def runIntervalTrain(train_iter, train_iter_1, eval_iter, path, ignore_index: in
         if p.dim() > 1:
             nn.init.xavier_uniform_(p)
 
-    # Define loss function : we ignore logits which are padding tokens
-    loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.98), eps=1e-9)
     
-    # Save history to dictionnary
-    history = {
-        'train_loss': [],
-        'eval_loss': [],
-        'train_acc': [],
-        'eval_acc': []
-    }
-
-    # Main loop
     print("=== ROUND 1 ===")
-    epochsToRun = 2
-    for epoch in range(1, epochsToRun):
-        start_time = time.time()
-        train_loss, train_acc, hist_loss, hist_acc = train(model, optimizer, 
-                                dataloader_train, loss_fn, epoch, ignore_index)
-        history['train_loss'] += hist_loss
-        history['train_acc'] += hist_acc
-        end_time = time.time()
-        
-        val_loss, val_acc, hist_loss, hist_acc = evaluate(model, dataloader_val, 
-                                                          loss_fn, ignore_index)
-        history['eval_loss'] += hist_loss
-        history['eval_acc'] += hist_acc
-        print((f"Epoch: {epoch}, Train loss: {train_loss:.3f}, Train acc: {train_acc:.3f}, Val loss: {val_loss:.3f}, Val acc: {val_acc:.3f} "f"Epoch time = {(end_time - start_time):.3f}s"))
-        
-
-    functions.plot_multiple_lists(history['train_acc'][5:], history['eval_acc'][5:],
-                                  xlabel='Batch Iteration', ylabel='Accuracy', 
-                                labels=['train_acc','eval_acc'], title='First round of training')
-
     # Save history to dictionnary
-    history = {
-        'train_loss': [],
-        'eval_loss': [],
-        'train_acc': [],
-        'eval_acc': []
-    }
-
-    # ROUND 2
+    run(model, optimizer, dataloader_train, dataloader_val, ignore_index,
+        title='First round of training')
+    
     print("=== ROUND 2 ===")
-    epochsToRun = 2
-    for epoch in range(1, epochsToRun):
-        start_time = time.time()
-        train_loss, train_acc, hist_loss, hist_acc = train(model, optimizer, 
-                                dataloader_train_1, loss_fn, epoch, ignore_index)
-        history['train_loss'] += hist_loss
-        history['train_acc'] += hist_acc
-        end_time = time.time()
-        
-        val_loss, val_acc, hist_loss, hist_acc = evaluate(model, dataloader_val, 
-                                                          loss_fn, ignore_index)
-        history['eval_loss'] += hist_loss
-        history['eval_acc'] += hist_acc
-        print((f"Epoch: {epoch}, Train loss: {train_loss:.3f}, Train acc: {train_acc:.3f}, Val loss: {val_loss:.3f}, Val acc: {val_acc:.3f} "f"Epoch time = {(end_time - start_time):.3f}s"))
-        
-    print("dataloader_val dataset", dataloader_val.dataset)
-    print("=== DONE ===")
-    #evaluate2(model, dataloader_val, loss_fn, ignore_index)
-
-    functions.plot_multiple_lists(history['train_acc'][5:], history['eval_acc'][5:],
-                                  xlabel='Batch Iteration', ylabel='Accuracy', 
-                                labels=['train_acc','eval_acc'], title='Second round of training')
+    # Save history to dictionnary
+    run(model, optimizer, dataloader_train_1, dataloader_val, ignore_index,
+        title='Second round of training')
+    print("=== TEST ===")
+    evaluate2(model, dataloader_val_1)
 #     torch.save(model.state_dict(), path)
 
 
-def runperson():
+def runstring():
     reverseString=True
     path = "../out/stringreverse" + str(reverseString) + ".pt" 
     train_iter = GenerateNoMarkerDataset(6400, reverseString=reverseString)
@@ -330,18 +259,17 @@ def runperson():
     eval_iter_1 = FixedDataset(3, reverseString=reverseString, drop = 0)
     runTrain(train_iter, train_iter_1, eval_iter, eval_iter_1, path)    
     
-    # S=30
-    # L = 20
-    # path = "../out/intervals.csv"
-    # train_iter = IntervalsDataset(6400, path, 0,  S = S, L = L)
-    # train_iter_1 = IntervalsDataset(8400, path, 6400, S = S, L = L)
-    # eval_iter = IntervalsDataset(10006, path, 14800+100, S = S, L = L)
+def runperson():
+    S=30
+    L = 20
+    path = "../out/intervals.csv"
+    train_iter = IntervalsDataset(6400, path, 0,  S = S, L = L)
+    train_iter_1 = IntervalsDataset(8400, path, 6400, S = S, L = L)
+    eval_iter = IntervalsDataset(10006, path, 14800+100, S = S, L = L)
+    eval_iter_1 = IntervalsDataset(3, path, 14800+100, S = S, L = L)
     
-    # path = "../out/intervals.pt" 
-    # runIntervalTrain(train_iter, train_iter_1, eval_iter, path)
-    # print('train_iter', train_iter)
-    # print('train_iter_1', train_iter_1)
-    # print('eval_iter', eval_iter)
+    path = "../out/intervals.pt" 
+    runIntervalTrain(train_iter, train_iter_1, eval_iter, eval_iter_1, path)
     
 if __name__ == "__main__":
     runperson()
