71f15e0ad628133474af2e02ef874a086864d5dc
oshanker <oshanker@hotmail.com>
2024-06-08 13:21:25 -0700

    multi row

--- python/transformer/base_transformer_shanker/data/multipleIntervalsData.py
+++ python/transformer/base_transformer_shanker/data/multipleIntervalsData.py
@@ -5,27 +5,28 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
 np.random.seed(0)
-def tokens_to_str(tokens):
-    return "".join([chr(x+94) for x in tokens])
-
-
 
 class MultipleIntervalsDataset(Dataset):
-    S = 20
+    S = 10
     L = 10
-    def __init__(self, n_samples, path, offset, S =20, L = 10):
+    def __init__(self, n_samples, path, offset, S =10, L = 10):
         super(MultipleIntervalsDataset, self).__init__()
         self.arr = np.loadtxt(path,
                      delimiter=",", dtype=np.int_) 
-        # max_index = len(self.arr) - S - L
         
         self.n_samples = n_samples
         self.offset = offset
         self.S = S
         self.L = L
         self.row_len = len(self.arr[0,:])
+        self.col_len = len(self.arr[:,0])
         if self.row_len < self.S+self.L:
             raise IndexError(f"Size out of bounds: row len {self.row_len}")
+        my_index = self.n_samples - 1 + self.offset
+        ret = self.getRowCol(my_index)
+        my_row_index = ret[0]
+        if my_row_index >= self.col_len:
+            raise IndexError(f"Row Index out of bounds: {my_row_index}")
 
     def getRowCol(self, index):
         divisor = self.row_len-self.S-self.L+1
@@ -41,11 +42,13 @@ class MultipleIntervalsDataset(Dataset):
 
     def __getitem__(self, index):
         my_index = index + self.offset
-        max_index = my_index+self.S+self.L
-        if max_index >= len(self.arr):
-            raise IndexError(f"Index out of bounds: {index}")
-        return torch.tensor(self.arr[my_index :my_index+self.S], dtype=torch.int64), \
-            torch.tensor(self.arr[my_index+self.S :max_index], dtype=torch.int64)
+        ret = self.getRowCol(my_index)
+        my_row_index = ret[0]
+        my_col_index = ret[1]
+        if my_row_index >= self.col_len:
+            raise IndexError(f"Index out of bounds: {my_row_index}")
+        return torch.tensor(self.arr[my_row_index, my_col_index :my_col_index+self.S], dtype=torch.int64), \
+            torch.tensor(self.arr[my_row_index, my_col_index+self.S:my_col_index+self.S+self.L], dtype=torch.int64)
 
     def __str__(self):
         return f"MultipleIntervalsDataset:[ S: {self.S}, L: {self.L}, \
@@ -53,13 +56,16 @@ class MultipleIntervalsDataset(Dataset):
 
 def runpersonMain():
     path = "../../../out/integers.csv"
-    train_iter = MultipleIntervalsDataset(4, path, 0, S = 3, L =5 )
+    train_iter = MultipleIntervalsDataset(6, path, 0, S = 3, L =5 )
     dataloader_train = DataLoader(train_iter, batch_size=3)
     print("train_iter.size", len(train_iter))
     print (train_iter)
-    i = 1
-    for index in range(0,5):
-        print (train_iter.getRowCol(index))
+    for index in range(0,6):
+        print(f"=== {index} ===")
+        ret = train_iter.getRowCol(index)
+        print (ret)
+        print(train_iter[index])
+    # i = 1
     # for s, t in iter(dataloader_train):
     #     print(f"=== {i} ===")
     #     i = i + 1
