52bc6a4b8100d9967435eab32fe39ec2cc3e0388
O Shanker <oshanker@hotmail.com>
2026-09-23 19:11:08 -0700

    poly7 fit

--- oldriemann/src/main/riemann/Poly7.java
+++ oldriemann/src/main/riemann/Poly7.java
@@ -334,18 +334,21 @@ public class Poly7 implements Poly {
         System.out.println("max1 " + poly7.evalMax1());
         System.out.println("poly7 " + poly7);
         double position = poly7.getPositionMax( );
-        System.out.println("positionmax " + position);
         double eval = poly7.eval( position  );
-        System.out.println("eval " + eval);
-        System.out.println("positionmax " + poly7.getPositionMax2(  ));
+        System.out.println("positionmax0 " + position + " eval " + eval);
+        System.out.println("positionmax1 " + poly7.getPositionMax2(  ));
         poly7.solveCoefficients(-1, 1, 8);
         System.out.println("============= " );
     	
     }
     
     public static void main(String[] args) {
-    	//zetaZeroFit(243.8749480149, 244.15890691298068, 244.3675025848634);
-    	testExactNoMax(); 
+    	zetaZeroFit(
+    			243.8749480149, 244.15890691298068, 244.3675025848634, 
+    			17.619276585379914, -20.007604626096071598, 19.343950349024609636, 
+    			1.9266754104451154, -1.232146174810101691
+    			);
+    	//testExactNoMax(); 
 /*
         //B = 0.5
         poly7 = new Poly7(0, 1, 2, 2, -1, 2,
@@ -368,16 +371,17 @@ public class Poly7 implements Poly {
          */
     }
 
-	static void zetaZeroFit(double a0, double a1, double a2) {
+	static void zetaZeroFit(double a0, double a1, double a2, 
+			double d0, double d1, double d2, double m0, double m1) {
 		Poly7 poly7 = new Poly7(
     			a0,
     			a1,
-    			a2, 17.619276585379914,
-    			-20.007604626096071598,
-    			19.343950349024609636
+    			a2, d0,
+    			d1,
+    			d2
         );
-        poly7.setExtrema(     1.9266754104451154,
-        	     -1.232146174810101691,      0
+        poly7.setExtrema(     m0,
+        	     m1,      0
 
         );
         
@@ -385,12 +389,12 @@ public class Poly7 implements Poly {
         System.out.println("deviation " + deviation + " poly " + poly7);
         
         
-        double eval = poly7.eval( 244.02115917156451 );
-        System.out.println("eval " + eval);
-        System.out.println("positionmax " + poly7.getPositionMax( ));
-        eval = poly7.eval(  244.26475821746848  );
-        System.out.println("eval " + eval);
-        System.out.println("positionmax " + poly7.getPositionMax2(  ));
+        double positionMax0 = poly7.getPositionMax( );
+        double eval = poly7.eval( positionMax0 );
+        System.out.println("eval " + eval +" positionmax0 " + positionMax0);
+        double positionMax1 = poly7.getPositionMax2(  );
+        eval = poly7.eval(  positionMax1  );
+        System.out.println("eval " + eval + " positionmax1 " + positionMax1);
         poly7.tabulate(a0, a2, 10);
         System.out.println("============= " );
 	}
